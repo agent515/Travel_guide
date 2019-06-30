4d77f728b767a0b28cdf8feb7ee742f1d5dd49a5
agent515 <kokate.rohit17@gmail.com>
2019-06-30 13:34:41 +0530

    side-nav added

---
+++ Travel_guide/www/js/register.js
@@ -0,0 +1,10 @@
+function user_register() {
+    var fname = document.getElementById("fname").value
+    var lname = document.getElementById("lname").value
+    var email = document.getElementById("email").value
+    var pwd = document.getElementById("pwd").value
+    var cpwd = document.getElementById("cpwd").value
+    
+    
+    
+}
