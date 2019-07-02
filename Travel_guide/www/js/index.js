8d6482854edc03684c4baa79c8977210ac707547
agent515 <kokate.rohit17@gmail.com>
2019-07-02 12:02:31 +0530

    db problem solved & login-logout done

--- Travel_guide/www/js/index.js
+++ Travel_guide/www/js/index.js
@@ -1,30 +1,26 @@
-function sign_up() {
-    if( document.getElementById('checked').checked == false ){
-                alert("You need to accept our Terms and condition")
-            }
-    
-    else {
-        var db = openDatabase('travel_guide','1.0','Test DB',2*1024*1024);
-        var fname = $('#fname').val();
-        var lname = $('#lname').val();
-        var email = $('#email').val();
-        var pwd = $('#pwd').val();
-        var cpwd = $('#cpwd').val();
-        var location = $('#s1').val();
-        if(pwd != cpwd){
-            alert("Passwords do not match");
-        }
-        else{
-            db.transaction(function(tx){
-            tx.executeSql('CREATE TABLE IF NOT EXISTS USERS(fname varchar(25),lname varchar(25),email varchar(25)"+" PRIMARY KEY,password varchar(20),location varchar(25))');
-            tx.executeSql('INSERT INTO USERS VALUES(?,?,?,?,?)',[fname,lname,email,pwd,location]);
-            alert(results.rows.item(0).fname);
-        });
-//        executeSql('CREATE TABLE IF NOT EXISTS USERS(fname varchar(25),lname varchar(25),email varchar(25) unique,password"+" varchar(20),location varchar(25))');    
-//        alert("Sign up successful");
-        window.location.href = "index.html";
-        }
-        
-        }
 
+function flight_info(){
+    unirest.get("https://forteweb-airportguide-airport-basic-info-v1.p.rapidapi.com/get_airport?airport_id=KLAX")
+    .header("X-RapidAPI-Host", "forteweb-airportguide-airport-basic-info-v1.p.rapidapi.com")
+    .header("X-RapidAPI-Key", "b90d14fcd0msh89be01094cab7fap14e6a0jsn783bb007bd9a")
+    .end(function (result) {
+      console.log(result.status, result.headers, result.body);
+    });
+}
+
+function logout() {
+    window.localStorage.setItem('user','#');
+}
+
+
+var log_status = window.localStorage.getItem('user')
+//alert(log_status);
+if(log_status == '#')
+    {
+        document.getElementById("log_status").innerHTML="<a class=\"nav-link\" href=\"login.html\">Log in</a>";
+        document.getElementById("log_status_home").innerHTML="<a href=\"index.html\" aria-expanded=\"false\">Home</a>"
+    }
+else {
+    document.getElementById("log_status").innerHTML="<a onclick=\"logout()\" class=\"nav-link\" href=\"index.html\">Log out</a>";
+    document.getElementById("log_status_home").innerHTML="<a href=\"user_home.html\" aria-expanded=\"false\">Home</a>"
 }
