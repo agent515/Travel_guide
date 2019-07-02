763036fca6e8f6945e74db5b35694a9d5b8d0f01
agent515 <kokate.rohit17@gmail.com>
2019-07-02 22:20:18 +0530

    worked on the User_home & SP_home. Also added additional files(profile,plan a trip & your trips)

--- Travel_guide/www/js/index.js
+++ Travel_guide/www/js/index.js
@@ -8,19 +8,52 @@ function flight_info(){
     });
 }
 
-function logout() {
+function user_logout() {
     window.localStorage.setItem('user','#');
+    window.localStorage.setItem('loc','#');
+}
+
+function place_logout() {
+    window.localStorage.setItem('place','#');
+    window.localStorage.setItem('loc','#');
 }
 
 
-var log_status = window.localStorage.getItem('user')
+var user_log_status = window.localStorage.getItem('user');
+var place_log_status = window.localStorage.getItem('place');
 //alert(log_status);
-if(log_status == '#')
+if(user_log_status == '#' && place_log_status=='#')
     {
         document.getElementById("log_status").innerHTML="<a class=\"nav-link\" href=\"login.html\">Log in</a>";
+
         document.getElementById("log_status_home").innerHTML="<a href=\"index.html\" aria-expanded=\"false\">Home</a>"
     }
-else {
-    document.getElementById("log_status").innerHTML="<a onclick=\"logout()\" class=\"nav-link\" href=\"index.html\">Log out</a>";
-    document.getElementById("log_status_home").innerHTML="<a href=\"user_home.html\" aria-expanded=\"false\">Home</a>"
+else if(user_log_status != '#'){
+    document.getElementById("log_status").innerHTML="<a onclick=\"user_logout()\" class=\"nav-link\" href=\"index.html\">Log out</a>";
+    document.getElementById("log_status_home").innerHTML="<a href=\"user_home.html\" aria-expanded=\"false\">Home</a>";
+    document.getElementById("log_status_options").innerHTML="<li><a href=\"user_profile.html\">Profile</a></li><li><a href=\"user_plan_trip.html\">Plan a trip</a></li><li><a href=\"user_trip.html\">Your trips</a></li>"
+
 }
+else {
+    document.getElementById("log_status").innerHTML="<a onclick=\"place_logout()\" class=\"nav-link\" href=\"index.html\">Log out</a>";
+    document.getElementById("log_status_home").innerHTML="<a href=\"SP_home.html\" aria-expanded=\"false\">Home</a>";
+    document.getElementById("log_status_options").innerHTML="<li><a href=\"SP_profile.html\">Profile</a></li>"
+    
+}
+
+var loc = window.localStorage.getItem("loc");
+function search() {
+    var loc = $("#search_location").val();
+    window.localStorage.setItem("loc",loc);
+    if(loc != '#')
+        {
+            document.getElementById("user_home_content").innerHTML="Places which matched your search.";
+        }
+    else {
+        //by default all the places dispplayed here will be near the user's location.
+        document.getElementById("user_home_content").innerHTML="<h4>Places near you.</h4>";
+    }
+    
+}
+
+    
---
+++ Travel_guide/www/js/place_home.js
@@ -0,0 +1,10 @@
+var loc = window.localStorage.getItem("loc");
+
+if(loc != '#')
+        {
+            document.getElementById("place_home_content").innerHTML="<h4>Places which matched your search.</h4>";
+        }
+else {
+        //by default all the places dispplayed here will be near the user's location.
+        document.getElementById("place_home_content").innerHTML="<h4>Places near you.</h4>";
+    }
